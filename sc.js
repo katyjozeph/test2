667ddadacce072c8b2a16a290142d8c3325d83be
katyjozeph <167994570+katyjozeph@users.noreply.github.com>
2024-05-09 14:47:08 +0300

    Add files via upload

--- sc.js
+++ sc.js
@@ -17,3 +17,29 @@ document.addEventListener('DOMContentLoaded', function () {
     setInterval(nextSlide, 3000);
     showSlide(currentSlide);
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const favoriteBtns = document.querySelectorAll('.favorite-btn');
+
+    favoriteBtns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            const card = this.closest('.Museum');
+            const id = this.getAttribute('data-id');
+            const title = card.querySelector('.card-title').textContent;
+            const rate=card.querySelector('.rate');
+            const imageSrc = card.querySelector('.card-img-top').getAttribute('src');
+
+            // Create favorite item object
+            const favoriteItem = {
+                id: id,
+                title: title,
+                imageSrc: imageSrc,
+                rate :rate
+            };
+
+            // Save favorite item to localStorage
+            let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+            favorites.push(favoriteItem);
+            localStorage.setItem('favorites', JSON.stringify(favorites));
+        });
+    });
+});
